ba317fc7a6a7c7a73cc282375a9432c982d2444a
馬鈴薯小鬼 <malingshuxiaogui@IVANde-MacBook-Pro.local>
2022-01-21 17:07:05 +0800

    remid work

--- js/api_V3.js
+++ js/api_V3.js
@@ -71,7 +71,41 @@ function imgChange(Tem,wx,pcpn,t_Icon,w_Icon){
 function getTempatureId(tem,id){
     document.getElementById(id).innerHTML = tem + '℃';
 }
+//提醒 週報
+function TextColorChange(Tem,wind,pcpn){
 
+    if (Tem >= 26 ) {
+      //document.getElementById("temp").style.color = "red";
+      document.getElementById('tempContent').innerHTML = '依據中央氣象局發布氣象資料，'+ Today +'日車城鄉跟恆春鄉天氣偏'+Tem+
+       '℃(熱)，提醒農友應留意防範農作暑害。';
+    } else if (Tem >= 15 && Tem < 26 ) {
+      //document.getElementById("temp").style.color = "green";
+      document.getElementById('tempContent').innerHTML='溫度適宜';
+    } else  {
+      document.getElementById("temp").style.color = "blue";
+      document.getElementById('tempContent').innerHTML = '依據中央氣象局發布氣象資料，'+ Today +'日車城鄉跟恆春鄉天氣偏'+Tem+
+      '℃(冷)，提醒農友應留意防範農作寒害。';
+    }
+
+    if(pcpn>10){
+      //document.getElementById("rain").style.color = "red";
+      document.getElementById('rainContent').innerHTML = Today+'日。預估雨量大於10mm，提醒農友應留意農田排水，以減少農作損失';
+    }else{
+      //document.getElementById("rain").style.color = "green";
+      document.getElementById('rainContent').innerHTML='';
+    }
+
+
+
+    if (wind=='南風'){
+      //document.getElementById("wind").style.color = "red";
+      document.getElementById('windContent').innerHTML= Today+'日。有改吹南風的機率。請農友們注意洋蔥保護，減少農作損傷。';
+    }else{
+      //document.getElementById("wind").style.color = "green";
+      document.getElementById('windContent').innerHTML='';
+    }
+    
+}//end of TextColorChange
 //顯示相對應天氣狀況
 function showWeather(e) {
     //今天的顯示
@@ -80,9 +114,11 @@ function showWeather(e) {
     var dataT = dataObject.data[30].tempture;// 溫度
     var datawx = dataObject.data[30].weather_condition;// 氣象      
     var pcpn = dataObject.data[30].pcpn;
+    var dataWind = dataObject.data[30].wind_dir;//風向
     getTempatureId(dataT,"nowT");
     document.getElementById("nowwx").innerHTML = datawx;
     imgChange(dataT,datawx,pcpn,tIcon,wIcon);
+    TextColorChange(dataT,wind,pcpn);
 }// end of fun showWeather
 
 function showWeekweather(ticon,wicon,item){
@@ -90,7 +126,6 @@ function showWeekweather(ticon,wicon,item){
     var dataT = dataObject.data[item].tempture;// 溫度
     var datawx = dataObject.data[item].weather_condition;// 氣象      
     var pcpn = dataObject.data[item].pcpn;      
-
     imgChange(dataT,datawx,pcpn,ticon,wicon);
 
 }
